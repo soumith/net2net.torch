b1293c1bd15cab7cf8165ff81b452be5c7084276
Riddhiman Dasgupta <riddhiman.dasgupta@gmail.com>
2016-08-10 00:30:29 +0530

    Only insert layers if network is sequential, otherwise return sub-network

--- init.lua
+++ init.lua
@@ -218,7 +218,7 @@ n2n.deeper = function(net, pos, nonlin, bnormFlag)
 
    if bnormFlag then
       if net.modules[pos].output then
-         local input = net.modules[pos].output:float()
+         local input = net.modules[pos].output:clone():type(torch.getdefaulttensortype())
          bnorm:forward(input);
          bnorm.weight = torch.cinv(bnorm.save_std)
          bnorm.bias = bnorm.save_mean
@@ -235,11 +235,16 @@ n2n.deeper = function(net, pos, nonlin, bnormFlag)
    s:add(nonlin)
    s:add(m2)
 
-   net:remove(pos)
-   for i=#s,1,-1 do
-      net:insert(s:get(i),pos)
+   -- if base network is sequential, insert individual layers
+   -- otherwise insert a sub-network
+   if torch.type(net):find('Sequential') then
+      net:remove(pos)
+      for i=#s,1,-1 do
+         net:insert(s:get(i),pos)
+      end
+   else
+      net.modules[pos] = s
    end
-   -- net.modules[pos] = s
    return net
 end
 
